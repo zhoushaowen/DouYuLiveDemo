7c7db148812e548020f338a4fa5de42de7d704a7
zhoushaowen <zhoushaowen@readyidu.com>
2017-02-17 15:35:54 +0800

    初始化项目

---
+++ 斗鱼直播/斗鱼直播/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  斗鱼直播
+//
+//  Created by zhoushaowen on 2017/2/17.
+//  Copyright © 2017年 Yidu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
